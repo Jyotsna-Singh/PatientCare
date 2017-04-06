5755b819a7d729577d63602eacd239d2f6d31b19
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-04-06 08:35:23 +0530

    first commit

---
+++ src/View/Helper/FormatHelper.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\ORM\TableRegistry;
+
+class FormatHelper extends Helper
+{
+	public function getName($id, $resource){
+		$records = TableRegistry::get($resource);
+		$query = $records->find()->where(['id' => $id]);
+		return $query->first()->name;
+	}
+}
